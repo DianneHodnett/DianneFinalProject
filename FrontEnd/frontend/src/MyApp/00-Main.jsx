7a9537d4807ed37b963247edf90b0d46742c6093
Dianne <diannehodnett@gmail.com>
2018-05-10 08:14:02 -0600

    today

--- FrontEnd/frontend/src/MyApp/00-Main.jsx
+++ FrontEnd/frontend/src/MyApp/00-Main.jsx
@@ -9,35 +9,39 @@ import AddApartment from './02-AddApartment'
 import Update1 from './03-UpdateFloorPlan'
 import Delete1 from './04-DeleteApartment'
 
-export default class extends React.Component{
-    render(){
+Hello World
+console.log(Hello World)
 
-        return(
-            <ApolloProvider client={client}>
-                <HashRouter>
-                    <div className="MainContainer">
 
-                        <div className="menu1">
-                            <Link className="menuButton" to="/">All Apartments</Link>
-                            <Link className="menuButton" to="/2">Add Apartments</Link>
-                            <Link className="menuButton" to="/3">Update Floor Plan</Link>
-                            <Link className="menuButton" to="/4">Delete Apartment</Link>
-                        </div>
-        <hr/>
-
-                        <Route exact path="/" component={Apartments}/>
-                        <Route path="/2" component={AddApartment}/>
-                        <Route path="/3" component={Update1}/>
-                        <Route path="/4" component={Delete1}/>
-
-                        <br/>
-                        <hr/>
-                        <div>Dianne Hodnett</div>
-
-                    </div>
-                </HashRouter>
-            </ApolloProvider>
-
-        )
-    }
+// export default class extends React.Component{
+//     render(){
+//
+//         return(
+//             <ApolloProvider client={client}>
+//                 <HashRouter>
+//                     <div className="MainContainer">
+//
+//                         <div className="menu1">
+//                             <Link className="menuButton" to="/">All Apartments</Link>
+//                             <Link className="menuButton" to="/2">Add Apartments</Link>
+//                             <Link className="menuButton" to="/3">Update Floor Plan</Link>
+//                             <Link className="menuButton" to="/4">Delete Apartment</Link>
+//                         </div>
+//         <hr/>
+//
+//                         <Route exact path="/" component={Apartments}/>
+//                         <Route path="/2" component={AddApartment}/>
+//                         <Route path="/3" component={Update1}/>
+//                         <Route path="/4" component={Delete1}/>
+//
+//                         <br/>
+//                         <hr/>
+//                         <div>Dianne Hodnett</div>
+//
+//                     </div>
+//                 </HashRouter>
+//             </ApolloProvider>
+//
+//         )
+//     }
 }
