36950d44b64266163084472b4ed7832a1f6db288
Dianne <diannehodnett@gmail.com>
2018-05-09 23:27:58 -0600

    adding react-router-dom

---
+++ FrontEnd/frontend/src/MyApp/00-Main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { ApolloProvider } from "react-apollo"
+import { HashRouter, Route, Link } from 'react-router-dom'
+import './Style/style.css'
+import { client } from './PrismaEndPoint/EndPoint'
---
+++ FrontEnd/frontend/src/MyApp/PrismaEndPoint/EndPoint.jsx
@@ -0,0 +1,5 @@
+import ApolloClient from "apollo-boost"
+
+const client = new ApolloClient({uri: "https://us1.prisma.sh/dianne-hodnett-559d7a/BackEnd/dev" })
+
+export { client }
